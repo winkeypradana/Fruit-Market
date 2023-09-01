fc5e86d2e643900746d42adf43374c702634eb5d
winkeypradana <winkeypradana@MacBook-Pro-2.local>
2023-09-01 13:19:20 +0700

    update stock dan pembayaran buah

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+# Stock buah
+sApel = 20
+sJeruk = 40
+sAnggur = 60
+
 # Init harga
 priceApel = 10000
 priceJeruk = 15000
 priceAnggur = 20000
 
 # Input jumlah buah
-nApel = int(input('Input jumlah apel: '))
-nJeruk = int(input('Input jumlah jeruk: '))
-nAnggur = int(input('Input jumlah anggur: '))
+while True:
+    nApel = int(input('Input jumlah apel: '))
+    if nApel <=sApel:
+        break
+    else:
+        print(f'Stock apel yang tersedia hanya {sApel}')
+while True:
+    nJeruk = nJeruk = int(input('Input jumlah jeruk: '))
+    if nJeruk <= sJeruk:
+        break
+    else: 
+        print(f'Stock jeruk yang tersedia hanya {sJeruk}')
+while True:
+    nAnggur = int(input('Input jumlah anggur: '))
+    if nAnggur <= sAnggur:
+        break 
+    else:
+        print (f'Stock jeruk yang tersedia hanya {sAnggur}')
+
 
 # Hitung harga per buah
 totalPriceApel = nApel * priceApel
@@ -21,9 +42,22 @@ print (
     f'''
 Detail Belanja
 
-Apel : {nApel} x {priceApel}
-Jeruk : {nJeruk} x {priceJeruk}
-Anggur : {nAnggur} x {priceAnggur}
-Total : {priceTotal}
+Apel : {nApel} x {priceApel} = Rp {totalPriceApel}
+Jeruk : {nJeruk} x {priceJeruk} = Rp {totalPriceJeruk}
+Anggur : {nAnggur} x {priceAnggur} = Rp {totalPriceAnggur}
+Total : Rp {priceTotal}
 '''
 )
+
+while True:
+    npembayaran = (int(input('Silahkan masukan nominal uang untuk membayar: ')))
+    selisihpembayaran = npembayaran - priceTotal
+    if npembayaran < priceTotal:
+        print (f'Maaf jumlah kekurangan yang anda harus bayar adalah: Rp. {selisihpembayaran}')
+    elif npembayaran > priceTotal:
+        print(f'Terimakasih, berikut jumlah kembaliannya:Rp. {selisihpembayaran}')
+        break
+    else:
+        npembayaran == selisihpembayaran
+        print(f'Terimakasih')
+        break
